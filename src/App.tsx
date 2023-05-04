617df5108cabcc542ee13a206e1c42a107138a6e
Andrei Galitianu <andrei.galitianu@ciconia-systems.com>
2023-05-04 13:10:39 +0300

    initial config

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const elem = document.documentElement;
+function openFullscreen() {
+  if (elem?.requestFullscreen) {
+    elem.requestFullscreen();
+  }
+}
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="window-container">
+        <section
+          onClick={openFullscreen}
+          onDoubleClick={closeFullscreen}
+          className="window camera-view"
         >
-          Learn React
-        </a>
-      </header>
+          <img
+            className="camera-image"
+            src="http://admin:12345@192.168.1.100/Streaming/channels/1/preview"
+            width="100%"
+            height="100%"
+          ></img>
+        </section>
+        <section className="window power-view">Consumption View</section>
+        <section className="window history-view">Historical Data View</section>
+        <section className="window browser-view">Browser View</section>
+        <section className="window music-player-view">
+          Music Player View
+        </section>
+        <section
+          onClick={openFullscreen}
+          onDoubleClick={closeFullscreen}
+          className="window weather-view"
+        >
+          Weather View
+        </section>
+      </div>
     </div>
   );
 }
